7dc59b9bdab810d5918cb6bbafed491697ddd918
mausv <mausv@me.com>
2015-09-15 18:45:31 -0500

    Added version 1.3.1 Implemented better update functionality.

--- app/src/main/java/com/exgerm/register/LoginActivity.java
+++ app/src/main/java/com/exgerm/register/LoginActivity.java
@@ -126,8 +126,8 @@ public class LoginActivity extends Activity {
     //Progress Dialog
     private ProgressDialog pDialog;
 
-    //public static String main_url = "http://exgerm.marpanet.com/doselimovil/";
-    public static String main_url = "http://192.168.1.152/doseli/";
+    public static String main_url = "http://exgerm.marpanet.com/doselimovil/";
+    //public static String main_url = "http://192.168.1.152/doseli/";
 
     File fileDir;
 
